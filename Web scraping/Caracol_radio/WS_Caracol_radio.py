a730e58c90313a6422b67774c71c8a2ca90e020b
juansalg <srjc211@gmail.com>
2019-08-05 17:38:56 -0500

    05/08/2019 - WS_Caracol_radio
    Versión final (0) del archivo.

--- Web scraping/Caracol_radio/WS_Caracol_radio.py
+++ Web scraping/Caracol_radio/WS_Caracol_radio.py
@@ -9,77 +9,136 @@ from bs4 import BeautifulSoup as soup
 from requests import get
 import pandas as pd
 import time
+import re
 from IPython.core.display import clear_output
 from random import randint
 
+## %reset
 
-""" qt = *palabra clave*
-"""
-url = "https://caracol.com.co/tag/homicidios/a/1"
-html = get(url)
-htmlsoup = soup(html.content,'html5lib')
-time.sleep(randint(2,4))
+keyword = input("What is the keyword you wanna look up?(e.g 'paro' o 'huelga de maestros')\n")
 
 
-print(url)
-articles = htmlsoup.find_all('div', class_="module-item-content")
+titles = []
+links = []
+contents = []
+dates = []
+
+
+start_time = time.time()
+requests = 0
 count = 0
+pages = [str(i) for i in range(57,2000)]
 
-if len(articles) != 0:
-        for oneArticle in articles:
+
+for page in pages:
+
+    url = "https://caracol.com.co/tag/homicidios/a/" + page
+    html = get(url)
+    htmlsoup = soup(html.content,'html5lib')
+    time.sleep(randint(2,4))
+    requests += 1
+    elapsed_time = time.time() - start_time
+    print('Request: {}; Total time: {} min; Frequency: {} requests/s'.format(requests, 
+          round(elapsed_time/60, 3), round(requests/elapsed_time, 3)))
+    clear_output(wait = True)
+    
+    
+    print(url)
+    print("")
+    
+    articles = htmlsoup.find_all('div', class_="module-item-content")
+    
+    
+    if len(articles) != 0:
+        
+        if str(articles).find('https:') != -1:
             
-            link = oneArticle.h2.a['href']
-            if link.find('https:',0) == -1:
-                continue
-            title = oneArticle.h2.a.text
-            content = ''
-            html2 = ''
-            print(link)
-            while html2 == '':
-                try:
-                    html2 = get(link)
-                    break
-                except:
-                    print("Connection refused by the server")
-                    time.sleep(3)
-                    print("Let's try again...")
-                    continue
-            noodles = soup(html2.content,'html5lib')
-            date_prueba =  noodles.find("span")
-            count = count + 1
-            
-            """## Modificar class_
-            if  == None:
-                date noodles.find('abbr', "span")= "Unspecified"
-            else:
-                date = noodles.find('span', class_="published-at").text.strip()[:-12]           
-            content = noodles.find('div', attrs = {'class':'article-content'}) 
-            if content == None :
-                content = noodles.find('div', attrs = {'class':'content-modules'})
-            if content == None:
-                titles.append(title)
-                dates.append(date)
-                texto="Especial"
-                contents.append(texto)
-                links.append(link)
-                test_df=pd.DataFrame({'Titulo':titles,
-                              'Fecha':dates,
-                              'Contenido':contents,
-                              'Link':links})
-            else:
-                texto = ''
-                for textos in content.find_all('p'):
-                    texto += textos.getText()   
+            for oneArticle in articles:
                 
-                titles.append(title)
-                contents.append(texto)
-                dates.append(date)
-                links.append(link) 
-                test_df=pd.DataFrame({'Titulo':titles,
-                              'Fecha':dates,
-                              'Contenido':contents,
-                              'Link':links})
-                       
+                link = oneArticle.h2.a['href']
+                if link.find('https:',0) == -1:
+                    continue
+                
+                title = oneArticle.h2.a.text
+                content = ''
+                html2 = ''
+                print(link)
+                while html2 == '':
+                    try:
+                        html2 = get(link)
+                        break
+                    except:
+                        print("Connection refused by the server")
+                        time.sleep(3)
+                        print("Let's try again...")
+                        continue
+                    
+                count = count + 1
+                print(count)
+                print("")
+                    
+                noodles = soup(html2.content,'html5lib')
+                
+                date_0 =  noodles.find("div", class_="cnt-metas")
+                date_1 =  date_0.find_all("span")
+                date_1 =  date_0.find_all("span") [len(date_1) - 1]
+                date = str(date_1)
+                date = re.search("\n(.*) -", date).group(1)
+                
+        
+                if  date == None:
+                    date = "**Unspecified**"
+                ##else:
+                  ##  date = noodles.find('span', class_="published-at").text.strip()[:-12]
+                    
+                content = noodles.find('div', attrs = {'class':'cuerpo'})
+                
+                if content == None:
+                    titles.append(title)
+                    dates.append(date)
+                    texto="**Especial**"
+                    contents.append(texto)
+                    links.append(link)
+                    test_df=pd.DataFrame({'Titulo':titles,
+                                  'Fecha':dates,
+                                  'Contenido':contents,
+                                  'Link':links})
+                else:
+                    texto = ''
+                    for textos in content.find_all('p'):
+                        texto += textos.getText()
+                    
+                    if not texto:
+                        titles.append(title)
+                        dates.append(date)
+                        texto="**Texto vacio**"
+                        contents.append(texto)
+                        links.append(link)
+                        test_df=pd.DataFrame({'Titulo':titles,
+                                  'Fecha':dates,
+                                  'Contenido':contents,
+                                  'Link':links})
+                    else:
+                        
+                        titles.append(title)
+                        contents.append(texto)
+                        dates.append(date)
+                        links.append(link) 
+                        test_df=pd.DataFrame({'Titulo':titles,
+                                  'Fecha':dates,
+                                  'Contenido':contents,
+                                  'Link':links})
+    
+    
+        else:
+            test_df=pd.DataFrame({'Titulo':titles,
+                                  'Fecha':dates,
+                                  'Contenido':contents,
+                                  'Link':links})
+            print("There were no more articles found with your keyword")
+            break
+            
+           
     else:
         test_df=pd.DataFrame({'Titulo':titles,
                                   'Fecha':dates,
@@ -87,4 +146,3 @@ if len(articles) != 0:
                                   'Link':links})
         print("There were no more articles found with your keyword")
         break
-  """      
